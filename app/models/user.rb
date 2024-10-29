3254f2865f498cdd8cf76ee2447d321ecdfbfa5a
tasty-tea <mrarttermos@gmail.com>
2024-10-29 20:47:22 +0600

    Copied test task specification

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_many :user_interests
   has_many :user_skills
   has_many :interests, through: :user_interests
-  has_many :skills, through: :user_skills
+  has_many :skills, through: :user_skills, class_name: "Skil"
 end
